0fee4627f8ea0992c1112ab972155d646cf7c3c6
jawad-wpbrigade <jawad.wpbrigade@gmail.com>
2022-09-12 13:35:20 +0500

    added wordpress coding standards

--- post-contributors.php
+++ post-contributors.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The plugin bootstrap file
  *
@@ -8,14 +7,14 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://example.org
- * @since             1.0.0
- * @package           Meta_Box
+ * @link    http://example.org
+ * @since   1.0.0
+ * @package Meta_Box
  *
  * @wordpress-plugin
  * Plugin Name:       Post Contributors
  * Plugin URI:        http://example.org
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       Add Contributor meta box in editor plus shows selected contributors on frontend
  * Version:           1.0.0
  * Author:            jawad
  * Author URI:        http://example.org
@@ -38,29 +37,30 @@ if ( ! defined( 'WPINC' ) ) {
 define( 'POST_CONTRIBUTORS_VERSION', '1.0.0' );
 
 /**
- * class for custom meta box
- * 
- * for post contributors
+ * Class for custom meta box
+ *
+ * For post contributors
  */
+class Class_Post_Contributors {
 
-class class_post_contributors{
 
 	/**
-	 * hooking into add_meta_boxes
+	 * Hooking into add_meta_boxes
 	 * to add our contributors meta box
 	 */
-	public function __construct()
-	{
-		add_action( 'add_meta_boxes', array($this, 'contributor_meta_box') );
-		add_action( 'save_post',array($this, 'save_editor') );
-		add_filter( 'the_content', array($this, 'contributors_post_content'), 99 );
+	public function __construct() {
+		add_action( 'add_meta_boxes', array( $this, 'contributor_meta_box' ) );
+		add_action( 'save_post', array( $this, 'save_editor' ) );
+		add_filter( 'the_content', array( $this, 'contributors_post_content' ), 99 );
 	}
 
 	/**
-	 * outputing contributors on frontend
+	 * Outputing contributors on frontend
 	 * at the end of content
+	 *
+	 * @param object $content object contains the content of current post.
 	 */
-	public function contributors_post_content( $content ){
+	public function contributors_post_content( $content ) {
 
 		if ( get_post_type() === 'post' ) {
 
@@ -68,82 +68,86 @@ class class_post_contributors{
 
 			$contributors_id = get_post_meta( get_the_ID(), 'contributors-list', true );
 
-			$contributors = '';
-			
-			foreach ( $contributors_id as $contributor_id ) {
-				$user = get_user_by( 'ID', $contributor_id );
-				$link = get_author_posts_url( $user->ID, $user->user_nicename );
-				$contributors .=  "<div><a href = '$link'><p>$user->display_name</p></a></div>";
+			if ( empty( $contributors_id ) ) {
+				return $content;
 			}
-			$heading = "<h3>Contributors: </h3>";
-			$content =  $content . $heading . $contributors ;
+
+			$contributors = '';
+
+			foreach ( $contributors_id as $contributor_id ) {
+				$user          = get_user_by( 'ID', $contributor_id );
+				$link          = get_author_posts_url( $user->ID, $user->user_nicename );
+				$contributors .= "<div><a href = '$link'><p>$user->display_name</p></a></div>";
+			}
+			$heading = '<h3>Contributors: </h3>';
+			$content = $content . $heading . $contributors;
 			ob_end_clean();
-  			return $content;
+			return $content;
 		}
 	}
 
+
+
 	/**
-	 * saving checkboxes on post update/publish
+	 * Saving checkboxes on post update/publish
+	 *
+	 * @param post_id $post_id get the current post id.
 	 */
-	public function save_editor( $post_id ){
+	public function save_editor( $post_id ) {
 
-			$new_data = $_POST['contributors-array'];
+		$new_data = $_POST['contributors-array'] ?? array();
 
-			$old_data = get_post_meta( $post_id, 'contributors-list' );
-			// Update post meta
-			if( !empty( $old_data ) ){
+		$old_data = get_post_meta( $post_id, 'contributors-list' );
 
-				update_post_meta( $post_id, 'contributors-list', $new_data );
-				
-			} else {
-				add_post_meta( $post_id, 'contributors-list', $new_data );
-		
-			}
-		
+		// Update post meta.
+		if ( ! empty( $old_data ) ) {
+
+			update_post_meta( $post_id, 'contributors-list', $new_data );
+
+		} else {
+			add_post_meta( $post_id, 'contributors-list', $new_data );
+
+		}
+
 	}
 
 	/**
-	 * contributor meta box 
+	 * Contributor meta box
 	 * adding id, label, screen here
 	 */
-	public function contributor_meta_box()
-	{
-		add_meta_box( 'meta-id', 'Contributors', array($this, 'contributor_meta_box_html'), array('post') );
+	public function contributor_meta_box() {
+		add_meta_box( 'meta-id', 'Contributors', array( $this, 'contributor_meta_box_html' ), array( 'post' ) );
 	}
 
 	/**
-	 * contributor_meta_box callback function to 
+	 * Contributor_meta_box callback function to
 	 * output backend html inside post editor
 	 */
-	public function contributor_meta_box_html(){
+	public function contributor_meta_box_html() {
 
 		$contributors_list = get_post_meta( get_the_ID(), 'contributors-list', true ) ? get_post_meta( get_the_ID(), 'contributors-list', true ) : array();
 
-
-		//querying all the users
+		// querying all the users!
 		$user_query = new WP_User_Query(
 			array(
-			'number' => '-1',
-			'fields' => array(
-				'user_nicename',
-				'display_name',
-				'ID',
-			),
+				'number' => '-1',
+				'fields' => array(
+					'user_nicename',
+					'display_name',
+					'ID',
+				),
 			)
 		);
 
-		//storing query result in $contributors variable
+		// storing query result in $contributors variable.
 		$contributors = $user_query->get_results();
-	
 
-
-		//looping through all the results in $contributors
+		// looping through all the results in $contributors.
 		foreach ( $contributors as $contributor ) {
-			$checked_value = in_array( $contributor->ID, $contributors_list ) ? "checked" : "";
-			
-		
-			echo '<input type="checkbox" id="'.$contributor->ID.'" name="contributors-array[]" value= "'.$contributor->ID.'" '.$checked_value.'>
-			<label for="'.$contributor->ID.'">'.$contributor->display_name.'</label><br>';
+			$checked_value = in_array( $contributor->ID, $contributors_list, true ) ? 'checked' : '';
+
+			echo '<input type="checkbox" id="' . $contributor->ID . '" name="contributors-array[]" value= "' . $contributor->ID . '" ' . $checked_value . '>
+			<label for="' . $contributor->ID . '">' . $contributor->display_name . '</label><br>';
 
 		}
 
